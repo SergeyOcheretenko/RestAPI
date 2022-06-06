86374cd5a4385364e2f1873150cc22177899c572
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-06 19:08:11 +0300

    Add basic express structure

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('SERVER IS WORKING');
+import express from "express";
+import config from "./CONFIG.js";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json('Server is working');
+});
+
+app.listen(config.PORT, () => console.log('SERVER STARTED'));
