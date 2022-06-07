f3d4292ae67314c641bf38b5f15a51d23fe5ed8b
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-07 14:30:25 +0300

    Create POST handler

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import config from "./CONFIG.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.status(200).json('Server is working');
+    res.status(200).json('Server (get) is working');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json('Server (post) is working');
 });
 
 app.listen(config.PORT, () => console.log('SERVER STARTED'));
