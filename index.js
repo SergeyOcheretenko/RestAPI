332c3bed4852a78024f6547b9c7ce1d275a1284d
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-08 10:01:56 +0300

    Create Mongo Post creating

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./CONFIG.js";
+import Post from "./Post.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json('Server (get) is working');
+    res.json('Server (get) is working');
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).json('Server (post) is working');
+app.post('/', async (req, res) => {
+    try {
+        const { author, title, content, picture } = req.body;
+        const post = await Post.create({ author, title, content, picture });
+        res.json(post);
+    } catch (e) {
+        res.status(500).json(e);
+    }    
 });
 
 async function startApp() {
