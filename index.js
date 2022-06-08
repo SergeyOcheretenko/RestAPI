f775fb5b5f448083f2f41a30084ef91eb1a756b1
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-08 15:10:49 +0300

    Add router and PostController

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./CONFIG.js";
-import Post from "./Post.js";
+import router from './router.js';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json('Server (get) is working');
-});
-
-app.post('/', async (req, res) => {
-    try {
-        const { author, title, content, picture } = req.body;
-        const post = await Post.create({ author, title, content, picture });
-        res.json(post);
-    } catch (e) {
-        res.status(500).json(e);
-    }    
-});
+app.use('/api', router);
 
 async function startApp() {
     try {
